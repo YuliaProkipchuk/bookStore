29c1060f64ddfb6b15c66e0a4f92b78b1273cded
YuliaProkipchuk <yulayulayula2003@gmail.com>
2024-10-10 09:57:11 +0300

    upgraded book page and connected firebase with auth

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,14 +1,26 @@
-import classes from './Auth.module.css';
-
-export default function Login() {
+import classes from "./Auth.module.css";
+export default function Login({changeUserInput}) {
+  
   return (
     <>
       <div className={classes.auth_input}>
-        <input type="text" name="login" id="login" placeholder="" />
-        <label htmlFor="login"> Login</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder=""
+          onChange={changeUserInput}
+        />
+        <label htmlFor="email"> E-mail</label>
       </div>
       <div className={classes.auth_input}>
-        <input type="password" name="password" id="password" placeholder="" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder=""
+          onChange={changeUserInput}
+        />
         <label htmlFor="password"> Password</label>
       </div>
     </>
--- frontend/src/components/Auth/SignUp.jsx
+++ frontend/src/components/Auth/SignUp.jsx
@@ -1,18 +1,19 @@
 import classes from './Auth.module.css';
-export default function SignUp() {
-  // const a = new Hash
+
+export default function SignUp({changeUserInput}) {
+  
   return (
     <>
       <div className={classes.auth_input}>
-        <input type="text" name="login" id="login" placeholder="" />
-        <label htmlFor="login"> Login</label>
+        <input type="text" name="username" id="username" placeholder="" onChange={changeUserInput}/>
+        <label htmlFor="username"> Username</label>
       </div>
       <div className={classes.auth_input}>
-        <input type="text" name="email" id="email" placeholder="" />
+        <input type="text" name="email" id="email" placeholder="" onChange={changeUserInput}/>
         <label htmlFor="email"> E-mail</label>
       </div>
       <div className={classes.auth_input}>
-        <input type="password" name="password" id="password" placeholder="" />
+        <input type="password" name="password" id="password" placeholder="" onChange={changeUserInput}/>
         <label htmlFor="password"> Password</label>
       </div>
     </>
